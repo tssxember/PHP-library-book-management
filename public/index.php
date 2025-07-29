06ddd96890bc948335ff171367d1fa70d913abba
Vinny <vincent.fezzuoglio@protonmail.com>
2025-07-29 16:32:03 -0400

    changes

--- public/index.php
+++ public/index.php
@@ -49,7 +49,7 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
 // LOAD OUR PATHS CONFIG FILE
 // This is the line that might need to be changed, depending on your folder structure.
 require FCPATH . '../app/Config/Paths.php';
-// ^^^ Change this line if you move your application folder
+// ^^^ Change this line if you move your application folder  
 
 $paths = new Paths();
 
